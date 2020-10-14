f85d859e2123aaef34cbdd1deaf41e8ef0b00869
AustinMegier <60328872+AustinMegier@users.noreply.github.com>
2020-10-14 18:54:34 +1100

    new folders for appendices

---
+++ Data/queries/Appendix_A/join_for_diagrams_app_A.sql
@@ -0,0 +1,26 @@
+-- show me all musicians from each scene 
+
+-- ordered by register/subregister order?
+
+-- this is very poorly done. but it works for now.
+
+SELECT Scene_ID,
+	   Instrumentalist_ID,
+	   Instrument, 
+	   Instrumentalist.Scene_Order_left_to_right,
+	   Chironomist_ID,
+	   Gesture_ID,
+	   Hand_NF,
+-- IF/when I add the View for gesture combos into my create_database.sql, I can (cheat by)
+	-- replacing Gesture_ID with 
+	  -- Gesture_ID_Near || '+' || Gesture_ID_Far AS Gesture_ID_Near_Far,
+	  -- don't know if this will work.
+	   Chironomist.Scene_Order_left_to_right
+  FROM Scene
+  JOIN Instrumentalist USING (Scene_ID)
+  JOIN Chironomist USING (Scene_ID)
+  JOIN Chironomist_Gesture USING (Chironomist_ID)
+  -- JOIN gesture_combo USING (Chironomist_ID)
+ ORDER BY Scene_ID ASC, Instrumentalist_ID ASC, Chironomist_ID ASC, Hand_NF DESC 
+ 
+  ;
